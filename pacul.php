718648652ce3b272d341431758e1970c01d983d5
mgcell11 <55955993+mgcell11@users.noreply.github.com>
2020-11-01 01:48:18 +0700

    Update pacul.php

--- pacul.php
+++ pacul.php
@@ -1,8 +1,7 @@
 <?php
-echo "<font color='red'>GENERATOR NIK NOK</font>
-echo "Created By Adun Banding\n";
+include "function.php";
 @ini_set('output_buffering',0);
-$data = array('3213127112370008 | 3213120302110029', '3213127112370008 | 3213120302110029', '3213127112370008 | 3213120302110029', '3213127112370008 | 3213120302110029');
+$data = array('3213127112370008 | 3213120302110029');
 $data2 = $data[array_rand($data)];
 $data3 = explode(' | ', $data2);
 //$ktp = base64_encode($data3[0]);
@@ -11,7 +10,9 @@ $ktp = $data3[0];
 $kk = $data3[1];
 //$data4 = array('KTP' => $ktp, 'NO_KK' => $kk);
 //print(json_encode(array('src'=>'Hasil', 'result'=>$data4)));
-echo ".::[ Generator NIK NOK ]::.\n\n";
+echo         .date('[d-m-Y] [H:i:s]')."   \n";
+echo color("red","GENERATOR NIK NOK\n");
+echo color("red"," Adun Bandring\n");
 echo "No. KTP : ".$ktp."\n";
-echo "No. KK : ".$kk."\n";
+echo "No. KK  : ".$kk."\n";
 ?>
