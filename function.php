07cc46ab7468651fdfcfe8efc75a3e15b31cbe22
mgcell11 <55955993+mgcell11@users.noreply.github.com>
2020-11-01 01:49:48 +0700

    Create function.php

---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
+}
+function color($color = "default" , $text)
+    {
+        $arrayColor = array(
+            'grey'      => '1;30',
+            'red'       => '1;31',
+            'green'     => '1;32',
+            'yellow'    => '1;33',
+            'blue'      => '1;34',
+            'purple'    => '1;35',
+            'nevy'      => '1;36',
+            'white'     => '1;0',
+        );  
+        return "\033[".$arrayColor[$color]."m".$text."\033[0m";
+    }
+
+?>
