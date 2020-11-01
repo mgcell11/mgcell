1d1cdd28fd8d8f08a700a32f001dd4e8d5cbf653
mgcell11 <55955993+mgcell11@users.noreply.github.com>
2020-11-01 10:34:50 +0700

    Update pacul.php

--- pacul.php
+++ pacul.php
@@ -11,13 +11,14 @@ $ktp = $data3[0];
 $kk = $data3[1];
 //$data4 = array('KTP' => $ktp, 'NO_KK' => $kk);
 //print(json_encode(array('src'=>'Hasil', 'result'=>$data4)));
-echo color("green","|           GENERATOR NIK NOK\n");
-echo color("yellow","|            Code By MG Cell\n");
+echo " \n";
+echo color("green","           GENERATOR NIK NOK\n");
+echo color("yellow","            Code By MG Cell\n");
 echo "-\n";
 echo "|Time    :".date('[d-m-Y] [H:i:s]')."   \n";
-echo "-\n";
-echo "-\n";
-echo color("purple","|No. KTP : ".$ktp."\n");
-echo color("nevy","|No. KK  : ".$kk."\n");
+echo " \n";
+echo " \n";
+echo color("purple","No. KTP : ".$ktp."\n");
+echo color("nevy","No. KK  : ".$kk."\n");
 
 ?>
